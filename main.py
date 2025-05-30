3baa0de2646fd35b158f80d7d5dfeb976a62a1fd
bagelquant <eric.yanzhong.huang@gmail.com>
2025-05-29 18:50:09 -0700

    [Refactor] update process to use JSON configuration for API calls and create indexes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from bageltushare import download, update_by_code, update_by_date
 from bageltushare import create_index, create_log_table, get_engine
 from time import perf_counter
@@ -14,66 +15,49 @@ ENGINE = get_engine(HOST, PORT, USER, PASSWORD, DB)
 TOKEN = "f7ad1328a17b49b5b7d126cb3ef4ae00565cba3adc28eeeecabbeb78"
 
 
-def yearly_update() -> None:
-    """update once a year"""
-    # download and replace table
-    download(engine=ENGINE, token=TOKEN, api_name="trade_cal")
-    download(engine=ENGINE,
-             token=TOKEN,
-             api_name="stock_basic",
-             params={"list_status": "L, D, P"},
-             fields=[
-                 "ts_code",
-                 "symbol",
-                 "name",
-                 "area",
-                 "industry",
-                 "cnspell",
-                 "market",
-                 "list_date",
-                 "act_name",
-                 "act_ent_type",
-                 "fullname",
-                 "enname",
-                 "exchange",
-                 "curr_type",
-                 "list_status",
-                 "delist_date",
-                 "is_hs"
-             ])
-
-
-def monthly_update() -> None:
-    """update once a month"""
-    by_code_apis = [
-        "balancesheet",
-        "cashflow",
-        "income",
-    ]
-
-    for api in by_code_apis:
-        update_by_code(engine=ENGINE, token=TOKEN, api_name=api)
-        create_index(engine=ENGINE, table_name=api)
-
-
-def daily_update() -> None:
-    """update once a day"""
-    by_date_apis = [
-        "daily",
-        "adj_factor",
-    ]
-
-    for api in by_date_apis:
-        update_by_date(engine=ENGINE, token=TOKEN, api_name=api)
+def create_indexes(apis: list[str]) -> None:
+    """create indexes for the given apis"""
+    for api in apis:
+        print(f"Index created for {api}")
         create_index(engine=ENGINE, table_name=api)
 
 
 def main() -> None:
     """main function"""
     create_log_table(ENGINE)
-    # yearly_update()
-    # monthly_update()
-    daily_update()
+    with open("update_apis.json", encoding="utf-8") as f:
+        apis = json.load(f)
+
+    for api in apis["yearly"]:
+        print(f"====== Download {api} ======")
+        # check if the api have params or fields
+        if "params" in apis["yearly"][api]:
+            params = apis["yearly"][api]["params"]
+        else:
+            params = None
+        if "fields" in apis["yearly"][api]:
+            fields = apis["yearly"][api]["fields"]
+        else:
+            fields = None
+        download(engine=ENGINE, token=TOKEN, api_name=api, params=params, fields=fields)
+
+    for api in apis["monthly"]:
+        print(f"====== Updating {api} by code ======")
+        update_by_code(engine=ENGINE, token=TOKEN, api_name=api)
+
+    for api in apis["daily"]:
+        print(f"====== Updating {api} by date ======")
+        update_by_date(engine=ENGINE, token=TOKEN, api_name=api)
+
+    all_apis = []
+    for api in apis["yearly"]:
+        all_apis.append(api)
+    for api in apis["monthly"]:
+        all_apis.append(api)
+    for api in apis["daily"]:
+        all_apis.append(api)
+    create_indexes(all_apis)
+
 
 if __name__ == "__main__":
     start = perf_counter()
